90158524a038e794300e01597928c1bfdf1797e9
scioneio <65683531+scioneio@users.noreply.github.com>
2020-09-13 22:17:50 -0400

    oikjoss
    uhuhss

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,15 +2,14 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>http://scione.io</homePageLink>
+		<homePageLink>https://scione.io</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="http://scione.io/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://scione.io/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://scione.io/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="http://scione.io/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="http://scione.io/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://scione.io/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://scione.io/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://scione.io/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://scione.io/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://scione.io/wp-json/" />
 			</apis>
 	</service>
 </rsd>
-	rsd>
 	
